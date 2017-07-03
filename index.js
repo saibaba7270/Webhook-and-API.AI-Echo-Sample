38b8611c55242e75905f9408cc4d3e1bd8b451b7
saibaba7270 <saibaba025@gmail.com>
2017-07-03 15:18:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,20 +13,6 @@ restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
-    var options = {
-     host: 'fingpayap.tapits.in',
-     port: 8080,
-     path: '/fingpay/getPaymentTypeMasterData?userId=gati'
-   };
-
-http.get(options, function(resp){
-  resp.on('data', function(chunk){
-    //do something with chunk
-      console.log(chunk);
-  });
-}).on("error", function(e){
-  console.log("Got error: " + e.message);
-});
     return res.json({
         speech: speech,
         displayText: speech,
